8dec7b82666283313cb0ec754258947c5e9bfd80
Gareth Rushgrove <gareth@morethanseven.net>
2014-08-15 13:26:38 +0100

    extra example tests

--- test/digitalocean_expect/test.clj
+++ test/digitalocean_expect/test.clj
@@ -5,5 +5,19 @@
 
 (def nodes (droplets (env :digitalocean-client-id) (env :digitalocean-api-key)))
 
+(def creds {:client (env :digitalocean-client-id) :key (env :digitalocean-api-key)})
+
+(defn nodes-by-status [status]
+  (droplets-with-status creds status))
+
+(def stopped-nodes (nodes-by-status "stopped"))
+(def active-nodes (nodes-by-status "active"))
+
 ; check number of nodes
 (expect 2 (count nodes))
+
+; check number of stopped nodes
+(expect 0 (count stopped-nodes))
+
+; check node with specific name
+(expect 0 (count (droplet-by-name creds "brian")))
